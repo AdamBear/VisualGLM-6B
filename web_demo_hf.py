b3a0f397b7faf4acf90e775fc3340ba16ce1ba45
AdamBear <adamxiong@gmail.com>
2023-07-10 23:13:50 +0800

    u

--- web_demo_hf.py
+++ web_demo_hf.py
@@ -1,9 +1,9 @@
 from transformers import AutoModel, AutoTokenizer
 import gradio as gr
 
-tokenizer = AutoTokenizer.from_pretrained("/data/visualglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("/data/visualglm-6b", trust_remote_code=True).half().cuda()
-model = model.eval()
+# tokenizer = AutoTokenizer.from_pretrained("/data/visualglm-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("/data/visualglm-6b", trust_remote_code=True).half().cuda()
+# model = model.eval()
 
 import torch
 
